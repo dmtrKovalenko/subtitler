3fccb815dcd85ccccc646f7164c7f3fc71f570db
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-07-13 23:51:02 +0200

    fix: Remove groupBy usage

--- src/types.d.ts
+++ src/types.d.ts
@@ -18,27 +18,3 @@ declare module "./Promise.gen" {
 declare module "@rescript/core/src/Core__Promise.gen" {
   export type t<T> = Promise<T>;
 }
-
-interface ObjectConstructor {
-  /**
-   * Groups members of an iterable according to the return value of the passed callback.
-   * @param items An iterable.
-   * @param keySelector A callback which will be invoked for each item in items.
-   */
-  groupBy<K extends PropertyKey, T>(
-    items: Iterable<T>,
-    keySelector: (item: T, index: number) => K,
-  ): Record<K, T[]>;
-}
-
-interface MapConstructor {
-  /**
-   * Groups members of an iterable according to the return value of the passed callback.
-   * @param items An iterable.
-   * @param keySelector A callback which will be invoked for each item in items.
-   */
-  groupBy<K, T>(
-    items: Iterable<T>,
-    keySelector: (item: T, index: number) => K,
-  ): Map<K, T[]>;
-}
