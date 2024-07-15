b6174cbc8c350db23d6a23edd0354be322b91b24
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-07-15 20:26:14 +0200

    feat: Add more description to the browser not supported view

--- src/screens/LandingDropzone.tsx
+++ src/screens/LandingDropzone.tsx
@@ -150,39 +150,43 @@ export const LandingDropzone: React.FC<LandingDropzoneProps> = ({
               Let's go!
             </button>
           </div>
-          <div
-            aria-hidden
-            className="rounded-xl max-w-2xl mt-8 bg-yellow-50 p-4"
-          >
-            <div className="flex">
-              <div className="flex-shrink-0">
-                <ExclamationTriangleIcon
-                  aria-hidden="true"
-                  className="size-6 text-yellow-400"
-                />
-              </div>
-              <div className="ml-3">
-                <h3 className="font-medium text-yellow-800">
-                  Large file attention
-                </h3>
-                <div className="mt-1 text-sm text-yellow-700">
-                  <p>
-                    Your browser does not support{" "}
-                    <a
-                      target="_blank"
-                      rel="noopener noreferer"
-                      href="https://caniuse.com/mdn-api_window_showsavefilepicker"
-                      className="font-bold underline"
-                    >
-                      file system access
-                    </a>{" "}
-                    , so we do not recommend uploading large videos longer than
-                    10 minutes as your browser may not be able to save them.
-                  </p>
+          {!window.showSaveFilePicker && (
+            <div
+              aria-hidden
+              className="rounded-xl max-w-2xl mt-8 bg-yellow-50 p-4"
+            >
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <ExclamationTriangleIcon
+                    aria-hidden="true"
+                    className="size-6 text-yellow-400"
+                  />
+                </div>
+                <div className="ml-3">
+                  <h3 className="font-medium text-yellow-800">
+                    Large file attention
+                  </h3>
+                  <div className="mt-1 text-sm text-yellow-700">
+                    <p>
+                      Your browser does not support{" "}
+                      <a
+                        target="_blank"
+                        rel="noopener noreferer"
+                        href="https://caniuse.com/mdn-api_window_showsavefilepicker"
+                        className="font-bold underline"
+                      >
+                        file system access
+                      </a>
+                      , so we do not recommend uploading large videos (longer
+                      than 10 minutes or larger than 300mb) as your browser may
+                      not be able to save them and all the work will be lost in
+                      the void.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
         <div className="mt-auto flex gap-6">
           <a
